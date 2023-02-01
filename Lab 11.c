6441ca9fece856c4004ff8fe783eb8fe0ca54836
Pratik Raut <106788839+MrCodeJunkie-jr@users.noreply.github.com>
2023-02-01 10:13:17 +0545

    Update Lab 11.c

--- Lab 11.c
+++ Lab 11.c
@@ -1,38 +1,25 @@
 #include <stdio.h>
 
-void extended_gcd(int a, int b, int *x, int *y)
-{
-    if (b == 0)
-    {
+void extendedEuclideanAlgorithm(int a, int b, int *x, int *y) {
+    if (b == 0) {
         *x = 1;
         *y = 0;
         return;
     }
-
     int x1, y1;
-    extended_gcd(b, a % b, &x1, &y1);
+    extendedEuclideanAlgorithm(b, a % b, &x1, &y1);
     *x = y1;
     *y = x1 - (a / b) * y1;
 }
 
-int main()
-{
-    int a, b, c;
-    printf("Enter the coefficients for the Diophantine Equation: ");
-    scanf("%d %d %d", &a, &b, &c);
-
-    int x, y;
-    int gcd = extended_gcd(a, b, &x, &y);
-    if (c % gcd != 0)
-    {
-        printf("The Diophantine Equation has no integer solutions.\n");
-    }
-    else
-    {
-        x = x * c / gcd;
-        y = y * c / gcd;
-        printf("A solution to the Diophantine Equation is x = %d and y = %d.\n", x, y);
-    }
+int main() {
+    int a, b, x, y, gcd;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    extendedEuclideanAlgorithm(a, b, &x, &y);
+    gcd = a * x + b * y;
+    printf("gcd(%d, %d) = %d = %d * %d + %d * %d\n", a, b, gcd, a, x, b, y);
     return 0;
 }
-
